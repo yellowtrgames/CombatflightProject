1a663324a0d01840e047f6e1e1812e5d3037af32
yellowtrgames <76137756+yellowtrgames@users.noreply.github.com>
2021-04-21 00:49:18 -0700

    shooting aircraft
    AIRPLANE SHOOTING GAME feel free to use it if you want

---
+++ Assets/scripts/playermovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+
+    public float movementspeed = 0f;
+    Vector2 movement = new Vector2();
+    Rigidbody2D rb2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movecharacter();
+    }
+
+    private void movecharacter()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.Normalize();
+        rb2D.velocity = movement * movementspeed;
+    }
+}
---
+++ Assets/scripts/spawnasteroid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnasteroid : MonoBehaviour
+{
+
+    public Transform[] spawnpoints;
+    public GameObject[] randomposition;
+    public float  Startspawntime =10;
+    public float spawntime = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating( "Spawn", Startspawntime, spawntime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        int spawnpoints = Random.Range (0,3);
+        int randomposition = Random.Range (0,6);
+
+        Instantiate(this.randomposition[randomposition], this.spawnpoints[spawnpoints].position, this.spawnpoints[spawnpoints].rotation);
+        
+    }
+}
